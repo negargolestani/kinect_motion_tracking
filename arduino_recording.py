4b1fd7467cf49da5c112b69bb5904fdf7ba28a36
negar <golestani.negar@gmail.com>
2020-08-05 17:49:59 -0700

    arduinoports

--- arduino_recording.py
+++ arduino_recording.py
@@ -5,7 +5,7 @@ import serial
 ####################################################################################################################################################
 class ARDUINO(object):
     def __init__(self, ports, baudrate=9600):
-        sels.sers = list()
+        self.sers = list()
         for i, port in enumerate(ports):
             ser = serial.Serial(port, baudrate=baudrate)
             ser.close()
@@ -13,7 +13,7 @@ class ARDUINO(object):
             self.sers.append(ser)
         return
     ################################################################################################################################################
-    def read(self, scale=5/1024, return_as_dict=True):
+    def read(self, scale=5/1024, return_as_dict=False):
         time = datetime.now().time()
         vind = [float(ser.readline().decode()) * scale for ser in self.sers]
 
@@ -30,11 +30,11 @@ if __name__ == '__main__':
     
     dataset_name ='dataset_05'
     file_name = 'record_00'
-    record_time = 50
+    record_time = 40
 
     # Initialization
     arduino = ARDUINO([ 
-        '/dev/cu.usbserial-1420',
+        '/dev/cu.usbserial-1410',
         '/dev/cu.usbserial-1420'
         ])   
     arduino_file_path = get_arduino_file_path(dataset_name, file_name)
@@ -43,12 +43,13 @@ if __name__ == '__main__':
 
     # Show when arduino starts recording
     print('Recording is Started \n  Press "Esc" Key to Stop Recording')
-
+    arduino.read()
+    
     # Recording loop
-    start_time = datetime.combine(date.min, kinect.time)
-    while cv2.waitKey(1)!=27 and (datetime.combine(date.min, kinect.time) - start_time).total_seconds() < record_time:  
+    start_time = datetime.combine(date.min, arduino.time)
+    while cv2.waitKey(1)!=27 and (datetime.combine(date.min, arduino.time) - start_time).total_seconds() < record_time:  
         data_dict = arduino.read(return_as_dict=True)
-        data_df = time_df.append( data_dict , ignore_index=True)
+        data_df = data_df.append( data_dict , ignore_index=True)
 
     print('Recording is Finished \n Wait for Saving ...')    
     data_df.to_csv(arduino_file_path, index=False)        
