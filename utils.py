d3132ee8eedf9620d3d075b0eeb1fce66e20fc0d
Negar <golestani.negar@gmail.com>
2020-10-09 10:21:24 -0700

    updateKinect

--- utils.py
+++ utils.py
@@ -1,32 +1,3 @@
-# import cv2
-# import pickle
-# import ctypes
-# import os
-# import copy
-# import sys
-# import numpy as np
-# import pandas as pd
-# import itertools
-# import imutils
-# import json
-# import time as time_lib
-# import csv
-# import glob
-# import random
-# import matplotlib.pyplot as plt
-# from sklearn.cluster import KMeans
-# from pathlib import Path
-# from datetime import datetime, date, time, timedelta
-# from scipy import signal, interpolate, stats
-# from collections import defaultdict
-# from math import*
-# import pywt
-# from itertools import chain, combinations
-# from numpy.fft import fft, ifft, fft2, ifft2, fftshift
-# from ast import literal_eval
-
-# from keras.layers import Layer
-
 import numpy as np
 import pandas as pd
 import time as time_lib
@@ -46,10 +17,8 @@ from pykinect2 import PyKinectRuntime
 
 
 main_directory = str( Path(__file__).parents[1] )
-datime_format = '%H:%M:%S.%f'
 dataset_folder_name = 'datasets'
-synth_dataset_folder_name = 'synthetic_dataset'
-eps = 1e-12
+
 
 ####################################################################################################################################################
 def get_time_file_path(dataset_name, file_name):
@@ -64,22 +33,8 @@ def get_camera_space_file_path(dataset_name, file_name):
 def get_markers_file_path(dataset_name, file_name):
     return main_directory + '/' + dataset_folder_name + '/' + dataset_name + '/markers/' + file_name + '.csv'
 ####################################################################################################################################################
-def get_rfid_file_path(dataset_name, file_name):
-    return main_directory + '/' + dataset_folder_name + '/' + dataset_name + '/rfid/' + file_name + '.csv'
-####################################################################################################################################################
-def get_arduino_file_path(dataset_name, file_name):
-    return main_directory + '/' + dataset_folder_name + '/' + dataset_name + '/arduino/' + file_name + '.csv'
-####################################################################################################################################################
 def get_color_setting_file_path(dataset_name, file_name):
     return main_directory + '/' + dataset_folder_name +  '/' + dataset_name  + '/calibration_setting/' + file_name + '.pickle'
-####################################################################################################################################################
-def get_sys_info(dataset_name):
-    file_path = main_directory + '/' + dataset_folder_name +  '/' + dataset_name  + '/calibration_setting/sys_info.txt'
-    sys_info = pd.read_csv(file_path, delimiter='\t').replace({'None': None})
-    return sys_info
-####################################################################################################################################################
-def get_dataset_folder_path(dataset_name):
-    return main_directory + '/' + dataset_folder_name + '/' + dataset_name + '/data' 
 ####################################################################################################################################################
 ####################################################################################################################################################
 def create_folder(file_path):
